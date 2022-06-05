c1d1d07ba5af290dea502d1b6b85b373fc4ba73b
SydneyN2 <104081541+SydneyN2@users.noreply.github.com>
2022-06-05 17:43:30 +0700

    Create inoutsetup.cpp
    kinda found on internet, set up for sublime competitive programming

---
+++ inoutsetup.cpp
@@ -0,0 +1,9 @@
+ //int main() {
+  #ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin); //in.txt name can be changed
+    freopen("out.txt", "w", stdout); //in.txt name can be changed
+  #endif
+
+  //put at the begining of an int main()
+  //to get input and output from .txt
+  // input, output and the using file must be in same directory
