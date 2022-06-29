2784924ea24fca830e827f3688bc3c96c5b9b19e
SydneyN2 <104081541+SydneyN2@users.noreply.github.com>
2022-06-29 11:19:29 +0700

    Update store_things_file.cpp

--- store_things_file.cpp
+++ store_things_file.cpp
@@ -17,3 +17,63 @@ int main() {
 
 
 //=================================================
+
+#include <iostream>
+#include <cmath>
+
+
+// Define tenth_power() here:
+int tenth_power(int num){
+  return pow(num, 10);
+}
+
+int main() {
+  
+  std::cout << tenth_power(0) << "\n";
+  std::cout << tenth_power(1) << "\n";
+  std::cout << tenth_power(2) << "\n";
+  
+}
+
+
+//==================================================
+
+#include <iostream>
+#include <vector>
+
+// Define first_three_multiples() here:
+  std::vector<int> first_three_multiples(int num){
+  std::vector<int> v;
+  v.push_back(num);
+  v.push_back(num * 2);
+  v.push_back(num * 3);
+  return v;
+
+
+
+}
+
+
+int main() {
+  
+  for (int element : first_three_multiples(8)) {
+    std::cout << element << "\n";
+  }
+  
+}
+
+
+//==================================================
+
+
+
+
+
+
+
+
+
+
+
+
+
