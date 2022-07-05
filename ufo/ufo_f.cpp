f60c50000cd1cd8110fa8911a8f10de6cbec9725
SydneyN2 <104081541+SydneyN2@users.noreply.github.com>
2022-07-05 16:02:01 +0700

    Update ufo_f.cpp

--- ufo/ufo_f.cpp
+++ ufo/ufo_f.cpp
@@ -1,1 +1,7 @@
+//ufo_f.hpp
+#include <vector>
+void display_misses(int misses);
+void greet();
+void end_game(std::string answer, std::string codeword);
+void display_status(std::vector<char> incorrect, std::string answer);
 
